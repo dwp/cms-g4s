8b500e7302ffdb6f4171a5df1111c1e521b472dc
RossDBrown <ross.david.brown@gmail.com>
2021-05-28 08:13:59 +0100

    G4S Tool v1.0 for testing

--- app/routes.js
+++ app/routes.js
@@ -3,10 +3,9 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 // copy the const name and the file path to new version
-
-const telephonytool = require('./routes/telephonytool');;
+const g4s = require('./routes/g4s');;
 
 //copy the router use and update the sprint version
-router.use(telephonytool);
+router.use(g4s);
 
 module.exports = router
